886ea3e3ee162162a3e26b0c6284a8227355ad16
thieled <daniel.thiele@fu-berlin.de>
2025-12-20 19:15:54 +0100

    cleaning up scripts

---
+++ scripts/test_helpers.R
@@ -0,0 +1,9 @@
+source("scripts/new_issue.R")
+source("scripts/inline_issue.R")
+
+# Apply -------------------------------------------------------------------
+
+new_issue(year = "2025", issue = "155", overwrite = F)
+
+
+inline_issue(year = "2025", issue = "155")
